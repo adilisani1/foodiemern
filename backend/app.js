78039c8c11be506f9b0f01c4a7ef847403cad658
Muhammad Adil <adil.isani1@gmail.com>
2023-06-28 15:45:57 +0500

    change app.js

--- backend/app.js
+++ backend/app.js
@@ -9,13 +9,13 @@ const dishRouter = require('./routes/dishes');
 const userRouter = require('./routes/auth');
 const contactRouter = require('./routes/contact');
 
-require('dotenv').config();
+
 const app = express();
 const PORT = 5000;
 
-
-const uri = "mongodb+srv://adil:aadil%400321@cluster0.1ijveb9.mongodb.net/foodapplication";
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+require('dotenv').config();
+// const uri = "mongodb+srv://adil:aadil%400321@cluster0.1ijveb9.mongodb.net/foodapplication";
+mongoose.connect("mongodb+srv://adil:aadil%400321@cluster0.1ijveb9.mongodb.net/foodapplication")
     .then(() => console.log("DBConnection Successfull"))
     .catch((err) => console.log(err))
 
@@ -25,7 +25,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", dishRouter);
 app.use("/user", userRouter);
-app.use("/api", contactRouter);
+app.use("/", contactRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`)
