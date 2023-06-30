43158905d690a07d78f5205033706bab21ba4eab
Muhammad Adil <adil.isani1@gmail.com>
2023-06-30 13:39:24 +0500

    new-bk

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
-
 const cors = require('cors');
-const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishes');
-const userRouter = require('./routes/auth');
-const contactRouter = require('./routes/contact');
+// const userRouter = require('./routes/auth');
+// const contactRouter = require('./routes/contact');
 
 require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-
+const PORT = 5000;
 const uri = process.env.MONGODB_URI;
+
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("DBConnection Successfull"))
-    .catch((err) => console.log(err))
+    .catch((err) => console.log(err));
 
 app.use(express.json());
 
 app.use(cors());
-app.use(bodyParser.json());
+
 app.use("/api", dishRouter);
-app.use("/user", userRouter);
-app.use("/api", contactRouter);
+// app.use("/user", userRouter);
+// app.use("/api", contactRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
--- backend/routes/dishes.js
+++ backend/routes/dishes.js
@@ -1,41 +1,23 @@
 const express = require('express');
 const Dishes = require('../models/Dishes');
-const bodyParser = require('body-parser');
 
-// const dishesJSON = require('../../src/dishes.json');
+const dishRouter = express.Router();
 
-// Single routing
-const router = express.Router();
-
-const app = express()
-app.use(bodyParser.json())
-
-router.get("/dishes", async (req, res) => {
+dishRouter.get("/dishes", async (req, res) => {
     const dishes = await Dishes.find({})
-    res.send(dishes)
+    res.status(201).json(dishes)
 })
 
-router.post('/dishes', async (req, res, next) => {
+dishRouter.post('/dishes', async (req, res, next) => {
     try {
         const dishes = await Dishes.create(req.body);
         const saveDish = await dishes.save();
-        res.send(saveDish)
+        res.status(201).json(saveDish)
+
     } catch (error) {
         console.log(error)
     }
 });
 
-router.delete('/dishes/:id', async (req, res, next) => {
-    try {
-        const deletedDish = await Dishes.findByIdAndRemove(req.params.id);
-        if (!deletedDish) {
-            return res.status(404).send('Dish not found');
-        }
-        res.send(deletedDish);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send('Internal Server Error');
-    }
-});
 
-module.exports = router;
+module.exports = dishRouter;
