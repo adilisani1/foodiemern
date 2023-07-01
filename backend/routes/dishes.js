91e28562ad50914191ff217a14adbb38bcd294a7
Muhammad Adil <adil.isani1@gmail.com>
2023-07-01 16:58:27 +0500

    changed

--- backend/routes/dishes.js
+++ backend/routes/dishes.js
@@ -1,24 +1,19 @@
 const express = require('express');
-const Dishes = require('../models/Dishes');
-
+const Dishes = require('../models/dish');
 const dishRouter = express.Router();
 
-dishRouter.get("/dishes", async (req, res, next) => {
-    try {
-        const dishes = await Dishes.find();
-        res.status(200).json(dishes);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
+dishRouter.get("/dishes", async (req, res) => {
+    const dishes = await Dishes.find({})
+    res.status(201).json(dishes);
+})
 
-dishRouter.post("/dishes", async (req, res, next) => {
+dishRouter.post('/dishes', async (req, res, next) => {
     try {
-        const dish = new Dishes(req.body);
-        const savedDish = await dish.save();
-        res.status(201).json(savedDish);
+        const dishes = await Dishes.create(req.body);
+        const saveDish = await dishes.save();
+        res.status(201).json(saveDish)
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(404).json(error)
     }
 });
 
