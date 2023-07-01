9890c3cf0e40b28360f79720ff7ae21bf8c0a201
Muhammad Adil <adil.isani1@gmail.com>
2023-07-01 13:44:49 +0500

    changinglines

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
-const PORT = 5000;
-require('dotenv').config();
-const dishRouter = require('./routes/dishes');
+
+
 // const userRouter = require('./routes/auth');
 // const contactRouter = require('./routes/contact');
-
-
-
-// const uri = process.env.MONGODB_URI;
-
 const app = express();
+const PORT = 5000;
+const cors = require('cors');
+
+const dishRouter = require('./routes/dishes');
+
+require('dotenv').config();
+// const uri = process.env.MONGODB_URI;
 
 mongoose.connect("mongodb+srv://adil:aadil%400321@cluster0.1ijveb9.mongodb.net/foodapplication")
     .then(() => console.log("DBConnection Successfull"))
     .catch((err) => console.log(err));
 
-app.use(express.json());
-
 app.use(cors());
-
+app.use(express.json());
 app.use("/api", dishRouter);
 // app.use("/user", userRouter);
 // app.use("/api", contactRouter);
