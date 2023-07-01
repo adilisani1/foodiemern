b290b2125366b7cf6ab855e10db26120d97b33a6
Muhammad Adil <adil.isani1@gmail.com>
2023-07-01 13:29:03 +0500

    update

--- backend/app.js
+++ backend/app.js
@@ -26,5 +26,5 @@ app.use("/api", dishRouter);
 // app.use("/api", contactRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on port http://localhost:${PORT}`)
 });
