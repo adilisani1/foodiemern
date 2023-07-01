bdcd3f50584b4cb01e5ebb27b814e1ba2363d8b3
Muhammad Adil <adil.isani1@gmail.com>
2023-07-01 13:40:59 +0500

    create-remove

--- backend/routes/dishes.js
+++ backend/routes/dishes.js
@@ -4,18 +4,18 @@ const Dishes = require('../models/Dishes');
 const dishRouter = express.Router();
 
 dishRouter.get("/dishes", async (req, res) => {
-    const dishes = await Dishes.find({})
+    const dishes = await Dishes.find()
     res.status(201).json(dishes)
 })
 
 dishRouter.post('/dishes', async (req, res, next) => {
     try {
-        const dishes = await Dishes.create(req.body);
+        const dishes = await Dishes(req.body);
         const saveDish = await dishes.save();
         res.status(201).json(saveDish)
 
     } catch (error) {
-        console.log(error)
+        res.status(500).json(error)
     }
 });
 
